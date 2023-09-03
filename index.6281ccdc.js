f2884cd3b35e00664beddf4a111ed01d5b8fb5c4
IgorN1989 <107757685+IgorN1989@users.noreply.github.com>
2023-09-03 20:34:40 +0000

    Deploying to gh-pages from @ IgorN1989/goit-js-hw-10@2154d974282dac4260db938753e77c08314f3833 🚀

---
+++ index.6281ccdc.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector(".breed-select"),document.querySelector(".cat-info  ");fetch("".concat("https://api.thecatapi.com/v1/breeds")).then((function(e){return e.json()})).then((function(e){return console.log(e)}))}();
+//# sourceMappingURL=index.6281ccdc.js.map
