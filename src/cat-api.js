13a513fc19c071bfc4e36ac4ca05ce3b8c286877
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-09-04 01:12:38 +0300

    add fetch

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,1 +1,16 @@
+function fetchCatsList(url) {
+  fetch(url)
+    .then(response => response.json())
+    .then(data =>
+      refs.select.insertAdjacentHTML('beforeend', addOptions(data))
+    );
+}
 
+function fetchCatByBreed(breedId) {
+  const URL = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&x-api-key=live_OLk1RycgcWv2xBkEUGVJtAj6KrGstFwR9QZRnnjj8HNyEgldoS7QordoNgkDvqzx`;
+  fetch(URL)
+    .then(response => response.json())
+    .then(data => console.log(data[0]));
+}
+
+export { fetchCatsList, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 // console.log(axios);
+import { fetchCatsList, fetchCatByBreed } from './cat-api';
+console.log(fetchCatsList);
+console.log(fetchCatByBreed);
 
 // axios.defaults.headers.common['x-api-key'] =
 //   'live_OLk1RycgcWv2xBkEUGVJtAj6KrGstFwR9QZRnnjj8HNyEgldoS7QordoNgkDvqzx';
@@ -15,6 +18,20 @@ const refs = {
 
 const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
 
-fetch(`${BASE_URL}`)
-  .then(response => response.json())
-  .then(data => console.log(data));
+// fetchCatsList(BASE_URL);
+
+refs.select.addEventListener('change', () => {
+  fetchCatByBreed(refs.select.value);
+
+  // createMarkup(fetchCatByBreed(refs.select.value));
+});
+
+function addOptions(arr) {
+  return arr
+    .map(({ id, name }) => `<option value="${id}">${name}</option>`)
+    .join('');
+}
+
+function createMarkup(arr) {
+  console.log(arr);
+}
