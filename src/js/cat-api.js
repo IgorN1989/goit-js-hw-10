08aa9f0043efd2299465a4645491f29161a4f4a2
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-09-05 03:29:41 +0300

    Update cat-api.js

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -5,15 +5,15 @@ const URL_OF_SEARCH = 'https://api.thecatapi.com/v1/images/search';
 const API_KEY =
   'live_OLk1RycgcWv2xBkEUGVJtAj6KrGstFwR9QZRnnjj8HNyEgldoS7QordoNgkDvqzx';
 
-const axios = require('axios');
-axios.defaults.headers.common['x-api-key'] = API_KEY;
+const axiosCat = axios.create();
+axiosCat.defaults.headers.common['x-api-key'] = API_KEY;
 
 function fetchBreeds() {
-  return axios.get(URL_OF_BREEDS).then(response => response.data);
+  return axiosCat.get(URL_OF_BREEDS).then(response => response.data);
 }
 
 function fetchCatByBreed(breedId) {
-  return axios
+  return axiosCat
     .get(`${URL_OF_SEARCH}?breed_ids=${breedId}`)
     .then(response => response.data)
     .then(data => {
